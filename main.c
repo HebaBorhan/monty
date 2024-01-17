0b91ef1748eee1f851f3a898ed9bef02063f0326
HebaZaki <h_borhan@hotmail.com>
2024-01-17 12:46:12 +0200

    entry point

--- main.c
+++ main.c
@@ -9,10 +9,18 @@
 
 int main(int argc, char **argv)
 {
-    (void)argv;
+    FILE *my_file;
+
     if (argc != 2)
     {
         dprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    my_file = fopen(argv[1], "r");
+    if (!my_file)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
         exit(EXIT_FAILURE);
     }
     return (0);
