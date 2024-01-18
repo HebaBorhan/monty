21a1bef05d935c33cd1bd20ecb3b305159b042b5
Heba <hebamagde@gmail.com>
2024-01-18 02:12:03 +0200

    update read_line

--- read_line.c
+++ read_line.c
@@ -14,7 +14,7 @@ void read_line(char *line, unsigned int line_number, stack_t **stack_head)
 {
 char *opcode = strtok(line, " \t\n");
 
-if (opcode == NULL || opcode[0] == '#')
+if (opcode[0] == '#')
 {
 return;
 }
