bf5e6d0d42a2e81548ff416d76d7c98c693e8372
HebaZaki <h_borhan@hotmail.com>
2024-01-18 15:30:00 +0200

    pop

--- pop.c
+++ pop.c
@@ -10,7 +10,7 @@ void pop(stack_t **stack, unsigned int line_number)
 stack_t *current = *stack;
 if (*stack == NULL)
 {
-fprintf(stderr, "L%u: can't pop, stack empty\n", line_number);
+fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
 free_stack(*stack);
 exit(EXIT_FAILURE);
 }
