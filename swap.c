d00dc40e9091ba90c27ab186a4121515db7dbfa4
HebaZaki <h_borhan@hotmail.com>
2024-01-18 20:34:24 +0200

    swap

--- swap.c
+++ swap.c
@@ -7,18 +7,18 @@
  */
 void swap(stack_t **stack, unsigned int line_number)
 {
-    stack_t *temp;
+stack_t *temp;
 
-    if (*stack == NULL || (*stack)->next == NULL)
-    {
-        fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+if (*stack == NULL || (*stack)->next == NULL)
+{
+fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+exit(EXIT_FAILURE);
+}
 
-    temp = (*stack)->next;
-    (*stack)->next = temp->next;
-    (*stack)->prev = temp;
-    temp->next = *stack;
-    temp->prev = NULL;
-    *stack = temp;
+temp = (*stack)->next;
+(*stack)->next = temp->next;
+(*stack)->prev = temp;
+temp->next = *stack;
+temp->prev = NULL;
+*stack = temp;
 }
