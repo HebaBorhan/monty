6cb743156f55b12ded28c89bd2de439c3a8b1b65
HebaZaki <h_borhan@hotmail.com>
2024-01-18 20:32:44 +0200

    mul

--- mul.c
+++ mul.c
@@ -7,17 +7,17 @@
  */
 void mul(stack_t **stack, unsigned int line_number)
 {
-    stack_t *temp;
+stack_t *temp;
 
-    if (*stack == NULL || (*stack)->next == NULL)
-    {
-        fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+if (*stack == NULL || (*stack)->next == NULL)
+{
+fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
+exit(EXIT_FAILURE);
+}
 
-    (*stack)->next->n *= (*stack)->n;
-    temp = *stack;
-    *stack = (*stack)->next;
-    (*stack)->prev = NULL;
-    free(temp);
+(*stack)->next->n *= (*stack)->n;
+temp = *stack;
+stack = (*stack)->next;
+(*stack)->prev = NULL;
+free(temp);
 }
