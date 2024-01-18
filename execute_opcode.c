88e3be09c2db603a8d0b03862202a40e8e85a6c3
HebaZaki <h_borhan@hotmail.com>
2024-01-18 16:44:19 +0200

    exec opcode

--- execute_opcode.c
+++ execute_opcode.c
@@ -14,7 +14,7 @@ int exec_opcode(char *opcode, unsigned int line_number, stack_t **stack_head)
 {
 int i;
 instruction_t inst[] = {{"push", push}, {"pall", pall}, {"pint", pint},
-{"pop", pop}, {"swap", swap}, {NULL, NULL}};
+{"pop", pop}, {"swap", swap}, {"nop", nop}, {NULL, NULL}};
 
 for (i = 0; inst[i].opcode && opcode; i++)
 {
