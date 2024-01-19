b6820dfee67c8c520b96a54a1658110af0a283b8
Heba <hebamagde@gmail.com>
2024-01-19 07:08:01 +0200

    rotl

---
+++ rotl.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+#include <stdio.h>
+/**
+ * rotl - Rotates the stack to the top
+ * @stack: Pointer to head of stack
+ * @line_number: Line number in Monty file
+ */
+void rotl(stack_t **stack, unsigned int line_number)
+{
+stack_t *current = *stack, *temp;
+if (*stack == NULL || *stack->next == NULL)
+return;
+temp = *stack->next;
+temp->prev = NULL;
+while (current->next != NULL)
+current = current->next;
+current->next = *stack;
+(*stack)->prev = current;
+(*stack)->next = NULL;
+*stack = temp;
+}
